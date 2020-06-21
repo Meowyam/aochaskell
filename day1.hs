b2b460d3e714d141931e4d4fedcefb5a5444583a
Meowyam <maryam@chilon.net>
2020-06-22 00:07:08 +0800

    combined functions that splits input into lines and changes strings to integers

--- day1.hs
+++ day1.hs
@@ -1,9 +1,7 @@
 import System.IO
-import Control.Monad
 
 main = do
  input <- readFile "day1input.txt"
- let inputLines = lines input
- let numInput = map (read::String->Integer) inputLines
+ let numInput = map (read::String->Integer) $ lines input 
  let fuel = sum $ map (subtract 2) (map (`div` 3) numInput)
  return fuel 
