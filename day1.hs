834d7dbd21078ebfbd7764d64feb099bab5ebe00
Meowyam <maryam@chilon.net>
2020-06-22 02:44:00 +0800

    day 1 part 2 complete

--- day1.hs
+++ day1.hs
@@ -1,9 +1,14 @@
 import System.IO
 
+totalFuel x
+  | eachFuel <= 0 = 0
+  | otherwise     = eachFuel + totalFuel eachFuel 
+  where eachFuel = ((subtract 2) . (`div` 3)) x
+
 main :: IO Integer
 
 main = do
   input <- readFile "day1input.txt"
   let numInput = map (read::String->Integer) $ lines input 
-  let fuel = sum $ map (subtract 2) (map (`div` 3) numInput)
+  let fuel = sum $ map totalFuel numInput
   return fuel 
