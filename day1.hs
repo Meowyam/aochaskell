5bfd9e9231d3b2a98390682099568879fe9101e5
Meowyam <maryam@chilon.net>
2020-06-22 02:08:42 +0800

    added type

--- day1.hs
+++ day1.hs
@@ -1,7 +1,9 @@
 import System.IO
 
+main :: IO Integer
+
 main = do
- input <- readFile "day1input.txt"
- let numInput = map (read::String->Integer) $ lines input 
- let fuel = sum $ map (subtract 2) (map (`div` 3) numInput)
- return fuel 
+  input <- readFile "day1input.txt"
+  let numInput = map (read::String->Integer) $ lines input 
+  let fuel = sum $ map (subtract 2) (map (`div` 3) numInput)
+  return fuel 
